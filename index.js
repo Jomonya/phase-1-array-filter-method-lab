16020c3348d23cc49324bf2318d9d84b9982a23e
Joy <joynandako1@gmail.com>
2024-03-28 03:47:51 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+const drivers = [
+    { name: 'Bobby', hometown: 'Pittsburgh' },
+    { name: 'Sammy', hometown: 'New York' },
+    { name: 'Sally', hometown: 'Cleveland' },
+    { name: 'Annette', hometown: 'Los Angeles' },
+    { name: 'Sarah', hometown: 'San Francisco' },
+    { name: 'bobby', hometown: 'Tampa Bay' }
+  ];
+  
+
+function findMatching(drivers, string) {
+    return drivers.filter(driver => driver.toLowerCase() === string.toLowerCase());
+  }
+  function fuzzyMatch(drivers, string) {
+    return drivers.filter(driver => driver.startsWith(string));
+  }
+  function matchName(drivers, string) {
+    return drivers.filter(driver => driver.name === string);
+  }
+  
